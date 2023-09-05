ba15e015d6f9ce2d16955b21668054a683e50163
davidszeder <143801826+davidszeder@users.noreply.github.com>
2023-09-05 23:41:29 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,70 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
 using Vizsgafeladat;
 
-var builder = new ConfigurationBuilder()
-.SetBasePath(Directory.GetCurrentDirectory())
-.AddJsonFile("appsettings.json");
-var configuration = builder.Build();
-var connectionString = configuration.GetConnectionString("ReportDbContext");
-var options = new DbContextOptionsBuilder<ReportDbContext>()
-    .UseSqlServer(connectionString)
-    .Options;
 
-using var dbContext = new ReportDbContext(options);
-var reports = dbContext.Reports.ToList();
-Console.WriteLine($"{reports.Count} darab bejegyzés található.");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("ReportDbContext");
+builder.Services.AddDbContext<ReportDbContext>(options => options.UseSqlServer(connectionString));
 
 
-Console.WriteLine("Írjon be körzetszámot, kerületszámot, vagy 100-nál kisebb számot (ahány nappal ezelőttig felvett feladatokat szeretné látni)!:");
-var input = Console.ReadLine();
+var app = builder.Build();
+app.UseRouting();
 
-if (int.TryParse(input, out int number))
-{
-    if (number < 100)
-    {
-        var dateThreshold = DateTime.Now.AddDays(-number);
-        reports = dbContext.Reports
-                             .Where(r => r.ReportDate >= dateThreshold)
-                             .ToList();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-        Console.WriteLine($"Az utóbbi {number} napban bejegyzett feladatok:");
-        foreach (var report in reports)
-        {
-            Console.WriteLine($"- {report.Address} került bejegyzésre {report.ReportDate}");
-        }
-    }
-    else if (number >= 101 && number <= 123)
-    {
-        string firstThreeDigits = number.ToString();
-
-        reports = dbContext.Reports
-                           .Where(r => r.ZipCode == null || r.ZipCode.StartsWith(firstThreeDigits))
-                           .ToList();
-
-        Console.WriteLine($"Feladatok a {number} körzet/kerületszámon:");
-        foreach (var report in reports)
-        {
-            Console.WriteLine($"- {report.Address} bejegyezve {report.ReportDate} napon");
-        }
-    }
-    else
-    {
-
-        string numberString = number.ToString();
-
-        reports = dbContext.Reports
-                           .Where(r => r.ZipCode == numberString || r.District == numberString)
-                           .ToList();
-
-        Console.WriteLine($"Feladatok a {number} körzet/kerületszámon:");
-        foreach (var report in reports)
-        {
-            Console.WriteLine($"- {report.Address} bejegyezve {report.ReportDate} napon");
-        }
-    }
-}
-else
-{
-    Console.WriteLine("Helytelen a bevitt adat. Kérem próbáljon számot megadni!");
-}
+app.Run();
